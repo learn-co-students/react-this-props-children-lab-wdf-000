3098d0babb01e93726d37ded4e0cb0b09b4e647f
Bibi Khadiza <limakhadiza@yahoo.com>
2016-12-23 17:11:28 -0500

    done

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const ReactDOM = require('react-dom');
 const Invitation = require('./components/Invitation');
 const ThemedParty = require('./components/ThemedDecorations');
 
+
+
 ReactDOM.render(
   <div>
-    <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
-    <ThemedParty theme="heaven">
-      <p>A decoration</p>
-      <p>Some other decoration</p>
-      <p>A last decoration</p>
-    </ThemedParty>
+  <Invitation>
+    Hey, you should totally come to this awesome party Im throwing!
+  </Invitation>
+  <ThemedParty theme="heaven">
+    <p>A decoration</p>
+    <p>Some other decoration</p>
+    <p>A last decoration</p>
+  </ThemedParty>
   </div>,
   document.getElementById('main')
 );

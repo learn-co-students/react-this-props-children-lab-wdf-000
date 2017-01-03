7a72f6eb60ca2f711a238f2d0bc53d7f207bae91
jin501 <soljin01@gmail.com>
2017-01-02 19:36:18 -0500

    Done.

---
+++ components/Invitation.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+const React = require('react')
+
+class Invitation extends React.Component {
+  render(){
+    return (
+      <div className='invitation'>
+      <h1>You've been invited!</h1>
+      <p>{this.props.children}</p>
+      </div>
+    )
+  }
+}
+
+module.exports = Invitation
---
+++ components/ThemedDecorations.js
@@ -0,0 +1,20 @@
+const React = require('react')
+
+class ThemedDecorations extends React.Component {
+  render() {
+    const childrenWithClass =
+    React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      })
+    })
+
+    return (
+      <div>
+        {childrenWithClass}
+      </div>
+    )
+  }
+}
+
+module.exports = ThemedDecorations

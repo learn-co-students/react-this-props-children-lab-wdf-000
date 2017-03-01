bfb5dab7e83008c950c8f5cd14ae219f9202bcec
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 12:48:20 -0500

    Working on the component

---
+++ components/Invitation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <h1></h1>
+    )
+  }
+}
+
+
+
+module.exports = Invitation;
